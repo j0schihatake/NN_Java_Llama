3ea3d9372daa14ad514847c1c85170a88e875973
j0schitadamori <j0schitadamori@gmail.com>
2023-10-23 13:47:35 +0300

    Добавил Spring проект который будет работать с библиотекой Llama.

---
+++ src/main/java/com/j0schi/java_llama/JavaLlamaApplication.java
@@ -0,0 +1,13 @@
+package com.j0schi.java_llama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLlamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLlamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j0schi/java_llama/JavaLlamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j0schi.java_llama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLlamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
